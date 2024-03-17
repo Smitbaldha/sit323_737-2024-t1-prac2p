c8adb8dffec6dd98810f60dd40bf0c816b784880
smitb <baldhasmit@gmail.com>
2024-03-17 13:33:30 +1100

    Basic changes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// Import the express library
 const express = require('express');
+// Import the path module to work with file paths
 const path = require('path');
 
+// Create a new express application
 const app = express();
+
+// Set the port number for our server
 const port = process.env.PORT || 8080;
 
-// sendFile will go here
-app.get('/new', function(req, res) {
+// Define a route for the home page ("/Home")
+app.get('/Home', function(req, res) {
+  // Send the index.html file as the response
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+// Start the server and listen on the specified port
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
